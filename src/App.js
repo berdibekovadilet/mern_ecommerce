8e33b2ed79b10c84d03f390306a196d3013ee185
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-05-17 13:00:36 +0600

    added pure react slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">
-    Test
-  </div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
